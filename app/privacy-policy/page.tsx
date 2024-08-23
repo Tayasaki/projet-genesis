0aece50364761a2d5a1bea5a158590f43dcb39b4
Tayasaki <enzo.jolidon@etu-hesge.ch>
2024-08-23 20:52:30 +0200

    doc: create tos and privacy-policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -4,5 +4,69 @@ import { redirect } from "next/navigation";
 export default async function PrivacyPolicy() {
   const session = await getAuthSession();
   if (!session?.user.id) redirect("/login");
-  return <div>PrivacyPolicy</div>;
+  return (
+    <div>
+      <h1>Politique de Confidentialité de Projet-Genesis</h1>
+      <div>
+        <h2>1. Introduction</h2>
+        <p>
+          Cette Politique de Confidentialité explique comment Projet-Gensis
+          (ci-après &lduoquotes;l&apos;application&rduoquotes;) collecte,
+          utilise, et protège les informations personnelles de ses utilisateurs.
+          En utilisant notre application, vous acceptez les pratiques décrites
+          dans cette politique.
+        </p>
+      </div>
+      <div>
+        <h2>2. Informations Collectées</h2>
+        <p>
+          L&apos;application utilise exclusivement l&apos;authentification via
+          OAuth pour permettre aux utilisateurs de se connecter. OAuth est un
+          protocole standardisé qui permet l&apos;autorisation sécurisée dans un
+          environnement simplifié. En utilisant OAuth, l&apos;application ne
+          stocke pas directement vos identifiants (comme les mots de passe) mais
+          reçoit un jeton d&apos;accès permettant de vérifier votre identité
+          auprès du fournisseur de service OAuth (comme Google, Facebook, etc.).
+        </p>
+        <p>
+          Lors de l&apos;authentification via OAuth, nous collectons et
+          utilisons uniquement les informations nécessaires à la création et à
+          la gestion de votre compte utilisateur sur notre application. Cela
+          inclut :
+        </p>
+        <ul>
+          <li>Nom et prénom (le cas échéant)</li>
+          <li>Adresse email</li>
+          <li>Photo de profil (facultatif, selon le fournisseur OAuth)</li>
+          <li>ID utilisateur fourni par le service OAuth</li>
+        </ul>
+      </div>
+      <div>
+        <h2>3. Sécurité des Données</h2>
+        <p>
+          Nous prenons la sécurité de vos données très au sérieux et mettons en
+          œuvre des mesures de sécurité techniques et organisationnelles
+          appropriées pour protéger vos informations contre l&apos;accès non
+          autorisé, la modification, la divulgation ou la destruction.
+        </p>
+        <p>
+          Toutes les communications entre l&apos;application et les serveurs
+          sont chiffrées via des protocoles de sécurité standard tels que
+          SSL/TLS. Les informations sensibles, comme les jetons d&apos;accès
+          OAuth, sont également protégées par des méthodes de chiffrement
+          avancées.
+        </p>
+      </div>
+      <div>
+        <h2>4. Modifications de la politique de confidentialité</h2>
+        <p>
+          Nous nous réservons le droit de modifier cette Politique de
+          Confidentialité à tout moment. En cas de modification substantielle,
+          nous vous en informerons par le biais de l&apos;application ou par un
+          autre moyen approprié. Nous vous encourageons à consulter
+          régulièrement cette politique pour rester informé des mises à jour.
+        </p>
+      </div>
+    </div>
+  );
 }
--- app/tos/page.tsx
+++ app/tos/page.tsx
@@ -4,5 +4,86 @@ import { redirect } from "next/navigation";
 export default async function TOS() {
   const session = await getAuthSession();
   if (!session?.user.id) redirect("/login");
-  return <div>Terms of services</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>Conditions générales d&apos;utilisation de Projet-Genesis</h1>
+      <div>
+        <h2>1. Introduction</h2>
+        <p>
+          Bienvenue sur Projet-Genesis, une application dédiée aux passionnés de
+          jeux de rôle (JDR). Cette application vous permet de créer des
+          scénarios et des personnages de manière manuelle ou semi-automatique.
+          Elle offre également des fonctionnalités complémentaires, telles que
+          la création d&apos;armes. Ces Conditions Générales d&apos;Utilisation
+          (CGU) régissent l&apos;utilisation de l&apos;application. En utilisant
+          cette application, vous acceptez ces CGU.
+        </p>
+      </div>
+      <div>
+        <h2>2. Acceptation des conditions</h2>
+        <p>
+          En accédant ou en utilisant Projet-Genesis, vous acceptez d&apos;être
+          lié par les présentes CGU. Si vous n&apos;acceptez pas ces conditions,
+          vous ne devez pas utiliser l&apos;application. Nous vous encourageons
+          à lire ces conditions attentivement avant d&apos;utiliser notre
+          service.
+        </p>
+      </div>
+      <div>
+        <h2>3. Accès et utilisation de l&apos;application</h2>
+        <p>
+          L&apos;application est destinée à toute personne âgée de 13 ans ou
+          plus. Si vous avez moins de 13 ans, vous devez obtenir la permission
+          d&apos;un parent ou d&apos;un tuteur pour utiliser cette application.
+          Vous êtes responsable de toute l&apos;activité qui se déroule sous
+          votre compte et vous devez vous assurer que toutes les informations
+          que vous fournissez lors de l&apos;inscription sont exactes et
+          complètes.
+        </p>
+      </div>
+      <div>
+        <h2>4. Comptes utilisateurs</h2>
+        <p>
+          Pour accéder à l&apos;application, vous devez créer un compte
+          utilisateur. Vous acceptez de fournir des informations précises et de
+          les mettre à jour en cas de modification. Vous êtes responsable de la
+          sécurité de votre compte et des actions effectuées via celui-ci.
+        </p>
+        <p>
+          En cas d&apos;activité suspecte sur votre compte, vous devez nous en
+          informer immédiatement. Nous nous réservons le droit de suspendre ou
+          de supprimer des comptes sans préavis en cas de violation de ces CGU.
+        </p>
+      </div>
+      <div>
+        <h2>5. Propriété intellectuelle</h2>
+        <p>
+          Tous les contenus disponibles sur l&apos;application, y compris les
+          textes, images, graphiques, logos, interfaces utilisateur, et le code
+          source, sont protégés par des droits d&apos;auteur et d&apos;autres
+          lois sur la propriété intellectuelle. L&apos;application et son
+          contenu sont la propriété exclusive de l&apos;équipe de développement.
+        </p>
+        <p>
+          Vous avez le droit d&apos;utiliser l&apos;application et son contenu
+          uniquement dans le cadre de l&apos;utilisation personnelle et non
+          commerciale de l&apos;application. Toute reproduction, distribution,
+          modification, ou autre utilisation du contenu sans autorisation
+          préalable est strictement interdite.
+        </p>
+      </div>
+      <div>
+        <h2>6. Modification des conditions</h2>
+        <p>
+          Nous nous réservons le droit de modifier ces CGU à tout moment. Si
+          nous apportons des modifications importantes, nous vous en informerons
+          via l&apos;application ou par tout autre moyen approprié. Votre
+          utilisation continue de l&apos;application après l&apos;entrée en
+          vigueur des modifications constitue votre acceptation de ces
+          modifications. Nous vous recommandons de consulter régulièrement ces
+          CGU pour rester informé de toute mise à jour.
+        </p>
+      </div>
+    </div>
+  );
 }
